6cfb8c719b270595bf9af277c3c01eab5facb5a1
Mircea Vutcovici <mirceavutcovici@gmail.com>
2015-11-18 15:59:32 -0500

    Update jmx.js

--- jmx.js
+++ jmx.js
@@ -41,3 +41,22 @@ print(Object.prototype.toString.call(attributeValue))
 
 
 print(Object.prototype.toString.call(attributeValue).slice(8, -1))
+
+// nested attributes using CompositeData and TabularData
+mbeanName="java.lang:type=GarbageCollector,name=G1 Young Generation"
+var objectName = new javax.management.ObjectName(mbeanName)
+var matchingNames = mbeanServerConnection.queryNames(objectName, null)
+attribute="LastGcInfo"
+attributeValue=mbeanServerConnection.getAttribute(objectName, attribute)
+key="memoryUsageAfterGc"
+memoryUsageAfterGc=attributeValue.get("memoryUsageAfterGc")
+//print(Object.prototype.toString.call(memoryUsageAfterGc))
+//print(memoryUsageAfterGc.keySet())
+tabularKeys = new StringArray(1)
+tabularKeys[0] = "G1 Old Gen"
+g1OldGen=memoryUsageAfterGc.get(tabularKeys)
+//print(Object.prototype.toString.call(g1OldGen))
+//print(g1OldGen.getCompositeType().keySet())
+//print(g1OldGen.get("value").getCompositeType().keySet())
+committed=g1OldGen.get("value").get("committed")
+print("committed = " + committed)

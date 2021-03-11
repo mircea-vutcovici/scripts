25f9713db8634c2a992565d958efe95da1725879
Mircea Vutcovici <mircea.vutcovici@redhat.com>
2021-03-11 12:42:32 -0500

    Added details about recaived responses

--- scapy-traceroute.py
+++ scapy-traceroute.py
@@ -8,15 +8,17 @@ if len(sys.argv) != 2:
 
 # we start with 1
 ttl = 1
-while 1:
+while True:
     #p=sr1(IP(dst=sys.argv[1],ttl=ttl)/ICMP(id=os.getpid()),verbose=0,timeout=3)       # ICMP traceroute (Windows)
     #p=sr1(IP(dst=sys.argv[1],ttl=ttl)/UDP(dport=53),verbose=0,timeout=3)              # UDP traceroute (Unix VJ)
     #p=sr1(IP(dst=sys.argv[1],ttl=ttl)/TCP(dport=80,flags="S"),verbose=0,timeout=3)    # TCPtraceroute
     p=sr1(IP(dst=sys.argv[1],ttl=ttl)/TCP(dport=80,flags="S",options=[('Timestamp',(0,0)),('SAckOK','')]),verbose=0,timeout=3)
     # if time exceeded due to TTL exceeded
     try:
+        # Check for ICMP type 11 (time-exceeded)
         if p[ICMP].type == 11 and p[ICMP].code == 0:
             print(ttl, '->', p.src)
+            print(p.summary())
             ttl += 1
         elif p[ICMP].type == 0:
             print(ttl, '->', p.src)

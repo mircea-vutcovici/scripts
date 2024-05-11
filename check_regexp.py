a1685cd86f42bbe11922e7069a7058673c2ddbe2
Mircea Vutcovici <mircea.vutcovici@redhat.com>
2024-05-10 23:45:31 -0400

    Validate RegEx

--- check_regexp.py
+++ check_regexp.py
@@ -6,6 +6,9 @@ if len(sys.argv) != 3:
     sys.exit('Usage: ' + sys.argv[0] + ' <regex> <string>')
 regex = sys.argv[1]
 string_to_match = sys.argv[2]
-if not re.match(regex, string_to_match):
-    sys.exit('ERROR: The string "' + string_to_match + '" is NOT matching the pattern "' + regex + '"')
-print('OK: The string "' + string_to_match + '" is matching the pattern "' + regex + '"')
+try:
+    if not re.match(regex, string_to_match):
+        sys.exit('ERROR: The string "' + string_to_match + '" is NOT matching the pattern "' + regex + '"')
+    print('OK: The string "' + string_to_match + '" is matching the pattern "' + regex + '"')
+except re.error:
+    sys.exit('ERROR: Invalid pattern "' + regex + '"')

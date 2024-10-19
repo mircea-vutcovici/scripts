9b6c864f75f418d9940127e14949e435f4654b13
Mircea Vutcovici <mircea.vutcovici@redhat.com>
2024-10-19 16:09:55 -0400

    Added comments and renamed variabes to be more readable

--- show-signal-queues-for-process.sh
+++ show-signal-queues-for-process.sh
@@ -1,14 +1,29 @@
 #read -p "PID=" pid
 pid=$1
-cat /proc/$pid/status|grep -E '(Sig|Shd)(Pnd|Blk|Ign|Cgt)'|while read name mask;do
-    bin=$(echo "ibase=16; obase=2; ${mask^^*}"|bc)
-    echo -n "$name $mask $bin "
+
+# Name of the item that had a signal mask in hex
+item_name=""
+
+# bitmask represented as a hex number
+item_hex_mask=""
+
+# For all lines that contains a signal bitmask for the process with PID $pid read the "queue" name and the bitmask
+cat /proc/$pid/status|grep -E '(Sig|Shd)(Pnd|Blk|Ign|Cgt)'|while read item_name item_hex_mask;do
+    # Transform the bit mask from hex to binary:
+    item_bin_mask=$(echo "ibase=16; obase=2; ${item_hex_mask^^*}"|bc)
+    echo -n "$item_name $item_hex_mask $item_bin_mask "
+    # We use i to iterate for each signal starting from 1 to when there are not anymore bits to check
     i=1
-    while [[ $bin -ne 0 ]];do
-        if [[ ${bin:(-1)} -eq 1 ]];then
+    # For each bit from the mask, starting with the least significant one
+    while [[ $item_bin_mask -ne 0 ]];do
+        # If the bit is set
+        if [[ ${item_bin_mask:(-1)} -eq 1 ]];then
+            # We print the signal's name
             kill -l $i | tr '\n' ' '
         fi
-        bin=${bin::-1}
+        # We remove the least significant bit from the mask:
+        item_bin_mask=${item_bin_mask::-1}
+        # We continue with next signal number
         set $((i++))
     done
     echo
